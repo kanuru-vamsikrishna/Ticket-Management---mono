3462f606eef942f0a88d0808ae19e7690a0fc20d
kanuru-vamsikrishna <kanuruvamsikrishna9999@gmail.com>
2025-07-14 09:26:09 +0530

    Add vite + react + typescript + tailwindcss setup for FE

---
+++ Client(FE)/Ticket Manager-FE/src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-grey-100">
+      <h1 className="text-xl font-bold text-blue-600">Vite + React + TS + Tailwind ✅</h1>
+    </div>
+  );
+}
+
+export default App
